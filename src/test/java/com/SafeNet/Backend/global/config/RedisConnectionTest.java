0ed7cc3594e237f0c8e33a98ded5890e1a1b24b8
khee2 <21keunghee@gmail.com>
2024-05-08 16:58:49 +0900

    [Feat] Redis 설정
    - 애플리케이션과 상호작용하기 위한, 메시지를 저장하기 위한 Redis 템플릿 각각 구현
    - Redis 동작 테스트

---
+++ src/test/java/com/SafeNet/Backend/global/config/RedisConnectionTest.java
@@ -0,0 +1,24 @@
+package com.SafeNet.Backend.global.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+public class RedisConnectionTest {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void testRedisConnection() {
+        stringRedisTemplate.opsForValue().set("testKey", "testValue");
+        String value = stringRedisTemplate.opsForValue().get("testKey");
+        assertEquals("testValue", value);
+    }
+}
