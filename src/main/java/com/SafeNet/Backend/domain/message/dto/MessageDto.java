94e538e56c0b8263b152834eecaf48479dc6f663
khee2 <21keunghee@gmail.com>
2024-05-09 02:58:12 +0900

    [Fix] JSON 역직렬화 중 모든 필드를 포함하는 생성자가 없어서 발생한 문제 해결
    - 수정한 곳: @NoArgsConstructor, @AllArgsConstructor 추가
    
    - 수정 이유: JSON 역직렬화에서 모든 필드값을 받을 수 있는 생성자가 필요함

--- src/main/java/com/SafeNet/Backend/domain/message/dto/MessageDto.java
+++ src/main/java/com/SafeNet/Backend/domain/message/dto/MessageDto.java
@@ -1,11 +1,15 @@
 package com.SafeNet.Backend.domain.message.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class MessageDto {
     private String sender;
