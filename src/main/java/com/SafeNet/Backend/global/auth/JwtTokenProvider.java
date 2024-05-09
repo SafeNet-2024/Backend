eda24b059e32cfb80121efe4a3d51cc1ad90e58e
chay3 <chay322p@gmail.com>
2024-05-10 05:58:57 +0900

    [Feat] Jwt관련 클래스 생성

---
+++ src/main/java/com/SafeNet/Backend/global/auth/JwtTokenProvider.java
@@ -0,0 +1,114 @@
+package com.SafeNet.Backend.global.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.log4j.Log4j2;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class JwtTokenProvider {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+
+    @Value("${spring.jwt.token.access-expiration-time}")
+    private long accessExpirationTime;
+
+    @Value("${spring.jwt.token.refresh-expiration-time}")
+    private long refreshExpirationTime;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    /**
+     * Access 토큰 생성
+     */
+    public String createAccessToken(Authentication authentication){
+        Claims claims = Jwts.claims().setSubject(authentication.getName());
+        Date now = new Date();
+        Date expireDate = new Date(now.getTime() + accessExpirationTime);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    /**
+     * Refresh 토큰 생성
+     */
+    public String createRefreshToken(Authentication authentication){
+        Claims claims = Jwts.claims().setSubject(authentication.getName());
+        Date now = new Date();
+        Date expireDate = new Date(now.getTime() + refreshExpirationTime);
+
+        String refreshToken = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+
+        // redis에 저장
+        redisTemplate.opsForValue().set(
+                authentication.getName(),
+                refreshToken,
+                refreshExpirationTime,
+                TimeUnit.MILLISECONDS
+        );
+
+        return refreshToken;
+    }
+
+    /**
+     * 토큰으로부터 클레임을 만들고, 이를 통해 User 객체 생성해 Authentication 객체 반환
+     */
+    public Authentication getAuthentication(String token) {
+        String userPrincipal = Jwts.parser().
+                setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody().getSubject();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userPrincipal);
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    /**
+     * http 헤더로부터 bearer 토큰을 가져옴.
+     */
+    public String resolveToken(HttpServletRequest req) {
+        String bearerToken = req.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    /**
+     * Access 토큰을 검증
+     */
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch(ExpiredJwtException e) {
+            log.error(EXPIRED_JWT.getMessage());
+            throw new BaseException(EXPIRED_JWT);
+        } catch(JwtException e) {
+            log.error(INVALID_JWT.getMessage());
+            throw new BaseException(INVALID_JWT);
+        }
+    }
+}
