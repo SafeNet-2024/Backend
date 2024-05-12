48f9bf7b4eabece33a1648e6477e6936d7fd08df
khee2 <21keunghee@gmail.com>
2024-05-12 19:38:30 +0900

    [Feat] 상품 저장, 상품 전체 조회, 상품 상세 조회, 파일 저장 로직 구현
    - 파일 저장과 관련된 책임을 FileStorageSerivce에 명확히 분리하여 재사용성 증가 및 PostService 로직에 집중 가능
    
    - FileStorageService에 로컬에 파일을 저장한 후 db에 메타데이터를 저장하는 로직 구현
    
    - PostService에는 상품 저장, 상품 전체 조회, 상품 선택 조회(상세 보기) 구현

---
+++ src/main/java/com/SafeNet/Backend/domain/post/dto/PostRequestDto.java
@@ -0,0 +1,22 @@
+package com.SafeNet.Backend.domain.post.dto;
+
+import com.SafeNet.Backend.domain.post.domain.Category;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PostRequestDto {
+    private Category category;
+    private String title;
+    private Integer cost;
+    private Integer count;
+    private String buyDate; // LocalDate 대신 String으로 받음
+    private String contents;
+    private MultipartFile receiptImage;
+    private MultipartFile productImage;
+}
---
+++ src/main/java/com/SafeNet/Backend/domain/post/dto/PostResponseDto.java
@@ -0,0 +1,25 @@
+package com.SafeNet.Backend.domain.post.dto;
+
+import com.SafeNet.Backend.domain.post.domain.Category;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PostResponseDto {
+    private Long postId;
+    private Category category;
+    private int likeCount;
+    private String imageUrl;
+    private String title;
+    private int count;
+    private String buyDate; // LocalDate 대신 String으로 받음
+    private String contents;
+    private String writer;
+    private int price;
+    private MultipartFile productImage;
+}
+
