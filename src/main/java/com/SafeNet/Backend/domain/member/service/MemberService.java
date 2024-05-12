09755e2c89541cefc9ef536cc71031cfc5ecdf73
khee2 <21keunghee@gmail.com>
2024-05-12 23:34:41 +0900

    [Feat] 상품 저장, 상품 전체 조회, 상품 상세 조회 api 생성

---
+++ src/main/java/com/SafeNet/Backend/domain/member/service/MemberService.java
@@ -0,0 +1,19 @@
+package com.SafeNet.Backend.domain.member.service;
+
+import com.SafeNet.Backend.domain.member.domain.Member;
+import com.SafeNet.Backend.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member findById(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() ->
+                new IllegalArgumentException("Member not found with id: " + memberId));
+    }
+}
