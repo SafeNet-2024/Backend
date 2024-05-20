ea47d32d7f2f7896ec7f187f871957b0b7c17653
khee2 <21keunghee@gmail.com>
2024-05-21 00:00:47 +0900

    [Feat] 키워드 및 카테고리 검색 api 구현 및 테스트
    - 글 제목 또는 설명으로 검색하거나, 카테고리로 검색하는 api 구현 및 테스트

---
+++ src/main/java/com/SafeNet/Backend/domain/post/api/PostSearchAndSortController.java
@@ -0,0 +1,34 @@
+package com.SafeNet.Backend.domain.post.api;
+
+import com.SafeNet.Backend.domain.post.dto.PostResponseDto;
+import com.SafeNet.Backend.domain.post.entity.Category;
+import com.SafeNet.Backend.domain.post.service.PostSearchAndSortService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
+@Tag(name = "Post Query", description = "Post Query API")
+public class PostSearchAndSortController {
+    private final PostSearchAndSortService postSearchAndSortService;
+
+    @GetMapping("/keyword")
+    public ResponseEntity<List<PostResponseDto>> searchByKeyWord(@RequestParam("keyword") String keyword) {
+        List<PostResponseDto> posts = postSearchAndSortService.searchByKeyWord(keyword);
+        return ResponseEntity.ok(posts);
+    }
+
+    @GetMapping("/category")
+    public ResponseEntity<List<PostResponseDto>> searchByCategory(@RequestParam("category") Category category) {
+        List<PostResponseDto> posts = postSearchAndSortService.searchByCategory(category);
+        return ResponseEntity.ok(posts);
+    }
+}
