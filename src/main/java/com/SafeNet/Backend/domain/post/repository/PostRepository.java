1288220bec6f3d36fc1335799a4bbdf99522077e
khee2 <21keunghee@gmail.com>
2024-05-21 00:00:47 +0900

    [Feat] 키워드 및 카테고리 검색 api 구현 및 테스트
    - 글 제목 또는 설명으로 검색하거나, 카테고리로 검색하는 api 구현 및 테스트

--- src/main/java/com/SafeNet/Backend/domain/post/repository/PostRepository.java
+++ src/main/java/com/SafeNet/Backend/domain/post/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.SafeNet.Backend.domain.post.repository;
 
+import com.SafeNet.Backend.domain.post.entity.Category;
 import com.SafeNet.Backend.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,11 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByMember_Id(Long memberId);
+
+    // 글 제목 또는 설명으로 검색
+    List<Post> findByTitleContainingOrContentsContaining(String title, String contents);
+
+    // 카테고리로 검색
+    List<Post> findByCategory(Category category);
 }
 
