5a470810fdcbd6f28bbbadc1fa93b2765b4b9013
khee2 <21keunghee@gmail.com>
2024-05-21 01:09:08 +0900

    [Refactor] 컨트롤러 응답 형식 일관성 개선
    - ResponseEntity를 사용하여 모든 컨트롤러 메서드에서 일관된 응답 형식 적용
    - PostController, PostSearchAndSortController, MemberItemController에 적용
    - 예외 처리 메서드 handleCustomException을 추가하여 일관된 오류 응답 처리

--- src/main/java/com/SafeNet/Backend/domain/post/api/MemberItemController.java
+++ src/main/java/com/SafeNet/Backend/domain/post/api/MemberItemController.java
@@ -3,9 +3,8 @@ package com.SafeNet.Backend.domain.post.api;
 import com.SafeNet.Backend.domain.post.dto.PostResponseDto;
 import com.SafeNet.Backend.domain.post.exception.PostException;
 import com.SafeNet.Backend.domain.post.service.MemberItemService;
-import com.SafeNet.Backend.domain.post.service.PostService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,30 +13,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/mypage")
 @RequiredArgsConstructor
+@Tag(name = "Member Item", description = "사용자가 찜한 게시글, 등록한 게시글 조회 API")
 public class MemberItemController {
-    private final PostService postService;
     private final MemberItemService memberItemService;
 
     @GetMapping("/my-posts")
     public ResponseEntity<?> getPostsByMemberId(@RequestParam("memberId") Long memberId) {
         List<PostResponseDto> posts = memberItemService.getPostsByMemberId(memberId);
         if (posts.isEmpty()) {
-            return new ResponseEntity<>("사용자가 등록한 게시물이 없습니다.", HttpStatus.OK);
+            return ResponseEntity.ok("사용자가 등록한 게시물이 없습니다.");
         }
-        return new ResponseEntity<>(posts, HttpStatus.OK);
+        return ResponseEntity.ok(posts);
     }
 
     @GetMapping("/liked-posts")
     public ResponseEntity<?> getLikedPostsByMemberId(@RequestParam("memberId") Long memberId) {
         List<PostResponseDto> likedPosts = memberItemService.getLikedPostsByMemberId(memberId);
         if (likedPosts.isEmpty()) {
-            return new ResponseEntity<>("등록된 찜이 없습니다.", HttpStatus.OK);
+            return ResponseEntity.ok("등록된 찜이 없습니다.");
         }
-        return new ResponseEntity<>(likedPosts, HttpStatus.OK);
+        return ResponseEntity.ok(likedPosts);
     }
-
     @ExceptionHandler(PostException.class)
     public ResponseEntity<String> handleCustomException(PostException e) {
-        return new ResponseEntity<>(e.getMessage(), e.getStatus());
+        return ResponseEntity.status(e.getStatus()).body(e.getMessage());
     }
 }
