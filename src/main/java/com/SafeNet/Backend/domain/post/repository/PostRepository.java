ad5a786e810f8b5ca59710d441eb84a121578b05
khee2 <21keunghee@gmail.com>
2024-06-04 21:05:57 +0900

    [fix] 잘못된 메서드 네이밍 수정 및 쿼리 리팩토링
    - findByRegionIdAndTitleOrContentsContaining 메서드 네이밍 오류 수정
    - JPQL을 사용하여 쿼리 명확하게 재작성

--- src/main/java/com/SafeNet/Backend/domain/post/repository/PostRepository.java
+++ src/main/java/com/SafeNet/Backend/domain/post/repository/PostRepository.java
@@ -2,7 +2,9 @@ package com.SafeNet.Backend.domain.post.repository;
 
 import com.SafeNet.Backend.domain.post.entity.Category;
 import com.SafeNet.Backend.domain.post.entity.Post;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,7 +20,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByRegion_IdOrderByBuyDateDesc(Long memberRegionId);
 
     List<Post> findByRegion_Id(Long regionId);
-    List<Post> findByRegion_IdAndTitleContainingOrContentsContaining(Long regionId, String title, String contents);
-
+    @Query("SELECT p FROM Post p WHERE p.region.id = :regionId AND (p.title LIKE %:keyword% OR p.contents LIKE %:keyword%)")
+    List<Post> findByRegionIdAndTitleOrContentsContaining(@Param("regionId") Long regionId, @Param("keyword") String keyword);
 }
 
