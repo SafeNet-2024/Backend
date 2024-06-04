fe60964471d240ea4dfbf85351ef048412d3b596
khee2 <21keunghee@gmail.com>
2024-06-04 15:03:13 +0900

    [feat] member 테이블에 임시의 region 정보 넣는 쿼리 생성 및 실행

---
+++ src/main/resources/update_region_data.sql
@@ -0,0 +1,6 @@
+-- 현재 프론트에서 region을 등록하는 과정이 구현이 안 되었기에, 임의의 값을 넣어놓습니다.
+update member set region_id = '1' where member_id = 5;
+update member set region_id = '15' where member_id = 6;
+update member set region_id = '356' where member_id = 7;
+update member set region_id = '378' where member_id = 8;
+update member set region_id = '405' where member_id = 9;
