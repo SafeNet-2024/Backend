8c238e8fe22c8ee3d9b5bfd29b2972dd0a7c69b8
khee2 <21keunghee@gmail.com>
2024-06-07 16:59:50 +0900

    [fix] /ws-stomp 경로 접근 허용

--- src/main/java/com/SafeNet/Backend/global/config/WebSockConfig.java
+++ src/main/java/com/SafeNet/Backend/global/config/WebSockConfig.java
@@ -1,30 +1,14 @@
 package com.SafeNet.Backend.global.config;
 
-import com.SafeNet.Backend.global.auth.JwtTokenProvider;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-
-import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
-
-    private final JwtTokenProvider jwtTokenProvider;
-
-    public WebSockConfig(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -35,30 +19,6 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*") // stomp websocket 연결
-                .addInterceptors(new HandshakeInterceptor() {
-                    @Override
-                    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-                        HttpHeaders headers = request.getHeaders();
-                        String token = headers.getFirst("ACCESS_TOKEN");
-                        if (token != null && token.startsWith("Bearer ")) {
-                            token = token.substring(7);
-                        } else {
-                            token = null;
-                        }
-                        if (jwtTokenProvider.validateToken(token)) {
-                            return true;
-                        } else {
-                            response.setStatusCode(HttpStatus.UNAUTHORIZED);
-                            return false;
-                        }
-                    }
-
-                    @Override
-                    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
-                    }
-                })
                 .withSockJS();
-
     }
-
 }
