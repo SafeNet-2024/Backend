81e10c8889f9431d4f4c77bded282c766df42a8b
khee2 <21keunghee@gmail.com>
2024-06-11 04:11:35 +0900

    [feat] WebSocket 메시지에 대한 JWT 토큰 검증 추가
    - WebSocket 연결 시 ACCESS_TOKEN을 검증하기 위해 AuthChannelInterceptor 구현
    - @MessageMapping("/chat/message")에서 각 메시지에 대해 토큰 유효성 검증 추가
    - AuthChannelInterceptor를 사용하도록 WebSocketConfig에 보안 설정 구성

--- src/main/java/com/SafeNet/Backend/global/config/WebSockConfig.java
+++ src/main/java/com/SafeNet/Backend/global/config/WebSockConfig.java
@@ -1,6 +1,7 @@
 package com.SafeNet.Backend.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +10,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
+    private final AuthChannelInterceptor authChannelInterceptor;
+
+    public WebSockConfig(AuthChannelInterceptor authChannelInterceptor) {
+        this.authChannelInterceptor = authChannelInterceptor;
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(authChannelInterceptor); // 웹소켓 검증을 위한 인터셉터 등록
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
