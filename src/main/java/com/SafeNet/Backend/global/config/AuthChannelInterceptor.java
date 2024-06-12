d116d46fa4817939eaaf75eea0a03d610368f3f0
khee2 <21keunghee@gmail.com>
2024-06-12 13:50:14 +0900

    [feat] websocket 연결 log 추가

--- src/main/java/com/SafeNet/Backend/global/config/AuthChannelInterceptor.java
+++ src/main/java/com/SafeNet/Backend/global/config/AuthChannelInterceptor.java
@@ -1,6 +1,7 @@
 package com.SafeNet.Backend.global.config;
 
 import com.SafeNet.Backend.global.auth.JwtTokenProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
@@ -15,6 +16,7 @@ import java.util.Collections;
 // WebSocket 메시지의 헤더에서 ACCESS_TOKEN을 추출하고 검증
 // 유효한 토큰이 있는 경우 사용자 인증 정보를 설정하고, 유효하지 않은 경우 연결을 차단
 @Component
+@Slf4j
 public class AuthChannelInterceptor implements ChannelInterceptor {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -27,18 +29,23 @@ public class AuthChannelInterceptor implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        if (StompCommand.CONNECT.equals(accessor.getCommand())) { // CONNECT 프레임은 서버에 대한 인증 및 기타 설정과 관련된 정보를 전송하기 위해 사용
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             String token = accessor.getFirstNativeHeader("ACCESS_TOKEN");
-            if (token != null && token.startsWith("Bearer ")) {
-                token = token.substring(7);
-                if (jwtTokenProvider.validateToken(token)) {
-                    String username = jwtTokenProvider.getAuthentication(token).getName();
-                    accessor.setUser(new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList()));
+            try {
+                if (token != null && token.startsWith("Bearer ")) {
+                    token = token.substring(7);
+                    if (jwtTokenProvider.validateToken(token)) {
+                        String username = jwtTokenProvider.getAuthentication(token).getName();
+                        accessor.setUser(new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList()));
+                    } else {
+                        throw new IllegalArgumentException("Invalid or expired token");
+                    }
                 } else {
-                    throw new IllegalArgumentException("Invalid or expired token");
+                    throw new IllegalArgumentException("Missing or invalid ACCESS_TOKEN header");
                 }
-            } else {
-                throw new IllegalArgumentException("Missing or invalid ACCESS_TOKEN header");
+            } catch (Exception e) {
+                log.error("Token validation failed: {}", e.getMessage());
+                throw e;
             }
         }
         return message;
